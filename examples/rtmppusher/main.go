2720bd287b5b364a0f2bed2a8b42fa9a980abb50
notedit <notedit@gmail.com>
2019-02-25 14:58:19 +0800

    add rtmp pusher

---
+++ examples/rtmppusher/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+
+	rtmp "github.com/notedit/rtmp-lib"
+	"github.com/notedit/rtmp-lib/flv"
+)
+
+func main() {
+
+	file, err := os.Open("test.flv")
+	if err != nil {
+		panic(err)
+	}
+
+	conn, _ := rtmp.Dial("rtmp://localhost/app/publish")
+
+	demuxer := flv.NewDemuxer(file)
+
+	streams, err := demuxer.Streams()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.WriteHeader(streams)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+
+		packet, err := demuxer.ReadPacket()
+
+		if err != nil {
+			break
+		}
+		conn.WritePacket(packet)
+	}
+
+	conn.WriteTrailer()
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,1 @@
 module github.com/notedit/rtmp-lib
-
-require (
-	github.com/hashicorp/go-multierror v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/sirupsen/logrus v1.3.0 // indirect
-	github.com/yutopp/go-amf0 v0.0.0-20180803120851-48851794bb1f // indirect
-	github.com/yutopp/go-rtmp v0.0.0-20190215174111-679b17dc8194
-)
