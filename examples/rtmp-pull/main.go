624da9324808a2e68dee198e2865d8ed06e824e5
notedit <notedit@gmail.com>
2019-04-11 19:56:37 +0800

    add rtmp pull

---
+++ examples/rtmp-pull/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"sync"
+
+	"github.com/notedit/rtmp-lib/av"
+
+	rtmp "github.com/notedit/rtmp-lib"
+	"github.com/notedit/rtmp-lib/pubsub"
+)
+
+type Channel struct {
+	que *pubsub.Queue
+}
+
+var channels = map[string]*Channel{}
+
+func pullStream(url *url.URL) {
+
+	fmt.Println(url.String())
+
+	conn, _ := rtmp.Dial(url.String())
+
+	_, err := conn.Streams()
+
+	if err != nil {
+		panic("got streams")
+	}
+
+	for {
+		packet, err := conn.ReadPacket()
+		if err != nil {
+			break
+		}
+		fmt.Println("got packet", packet.IsKeyFrame)
+	}
+
+}
+
+func main() {
+
+	l := &sync.RWMutex{}
+
+	server := &rtmp.Server{}
+
+	server.HandlePlay = func(conn *rtmp.Conn) {
+
+		l.RLock()
+		ch := channels[conn.URL.Path]
+		l.RUnlock()
+
+		if ch != nil {
+
+			cursor := ch.que.Latest()
+
+			streams, err := cursor.Streams()
+
+			if err != nil {
+				panic(err)
+			}
+
+			conn.WriteHeader(streams)
+
+			for {
+				packet, err := cursor.ReadPacket()
+				if err != nil {
+					break
+				}
+				conn.WritePacket(packet)
+			}
+		}
+	}
+
+	server.HandlePublish = func(conn *rtmp.Conn) {
+
+		l.Lock()
+		ch := channels[conn.URL.Path]
+
+		if ch == nil {
+			ch = &Channel{}
+			ch.que = pubsub.NewQueue()
+			ch.que.SetMaxGopCount(1)
+			channels[conn.URL.Path] = ch
+		}
+		l.Unlock()
+
+		var streams []av.CodecData
+		var err error
+
+		if streams, err = conn.Streams(); err != nil {
+			panic(err)
+		}
+
+		ch.que.WriteHeader(streams)
+
+		go pullStream(conn.URL)
+
+		for {
+			var pkt av.Packet
+			if pkt, err = conn.ReadPacket(); err != nil {
+				break
+			}
+
+			ch.que.WritePacket(pkt)
+		}
+
+		l.Lock()
+		delete(channels, conn.URL.Path)
+		l.Unlock()
+
+		ch.que.Close()
+
+	}
+
+	server.ListenAndServe()
+
+}
